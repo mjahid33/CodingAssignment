6bf6eb8b5efcf983882c63a116da08cacac4692d
Jahid Mammad <jahidmammadov2@gmail.com>
2023-08-16 16:16:25 -0500

    Added relevant comment

--- src/test/java/stepDefinitions/SkipStep.java
+++ src/test/java/stepDefinitions/SkipStep.java
@@ -24,6 +24,9 @@ public class SkipStep {
     	WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Skip')]")));
 
     	 sPage.skip.click();
+    	 
+    	 //As click navigated to a different page I had to use the next page URL.
+    	 
          driver.get("https://www.discovercard.com/application/website/apply?adpt=mn&srcCde=KXPA&ICMPGN=ALL_CC_SECURED_APPLY_NOW_BTN&_gl=1*1cvhr91*_ga*MjExOTI0MjU5Ny4xNjkyMTUzMDUw*_ga_3MJNPV4VSE*MTY5MjIwNTQ0My40LjEuMTY5MjIwNTQ0OC41NS4wLjA.&_ga=2.184617440.85601018.1692130023-2119242597.1692153050&adobe_mc=MCMID%3D87579089574437912910469389947897858942%7CMCORGID%3D0D6C4673527839230A490D45%2540AdobeOrg%7CTS%3D1692205453");
         
         	   	
